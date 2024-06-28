86270e64dc24721744fe4c29a34df8744474c22c
Mangesh Korde <143051332+mangesh-korde@users.noreply.github.com>
2024-06-28 22:25:14 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,16 @@
+#DataTypes...#
+
+a=5
+print(type(a))
+b=5.1
+print(type(b))
+c="college"
+print(type(c))
+d=[10,20,30]
+print(type(d))
+e=(10,20,30)
+print(type(e))
+f={10,20,30}
+print(type(f))
+g={"name":"om","college":"RBNB"}
+print(type(g))
